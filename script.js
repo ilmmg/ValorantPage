e9ae8df20d52034cd5a6e6a9c9e3bdb3e3cf71b1
ilmmg <mg1414@protonmail.com>
2023-08-31 23:36:46 -0300

    agrege ventana modal, solucionar error de vision

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-alert("hola")
+let openModal = document.getElementById('openModal');
+let modalAgente = document.getElementById('modal');
+let closeModal = document.getElementById('close');
+
+// Abri modal
+
+openModal.onclick = function () {
+    modalAgente.style.visibility = "visible";
+}
+
+// Cerrar Modal Btn
+
+closeModal.onclick = function () {
+    modalAgente.style.visibility = "hidden";
+}
+
+// Cerrar en ventana 
+
+modalAgente.onclick = function () {
+    modalAgente.style.visibility = "hidden";
+}
